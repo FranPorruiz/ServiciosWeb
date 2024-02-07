7d2975c3a5cc6043923f273cd475e3a5dfe40fff
FranPorruiz <franporruiz@gmail.com>
2024-02-07 20:58:13 +0100

    Actualizacion 1 y 3

--- 01_servicio_buscador/src/main/java/controller/BuscadorController.java
+++ 01_servicio_buscador/src/main/java/controller/BuscadorController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import model.Resultado;
 import service.interfaces.BuscadorService;
 //este metodo nos dice que que siempre va a haber una respuesta
+@CrossOrigin("*")//nos permite llamadas desde cualquier origen
 @RestController
 public class BuscadorController {
 	//con esto inyectamos la interfaz service
